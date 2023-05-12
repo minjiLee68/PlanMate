69435162af9b18b5e4988f2ab579830a42dc5dce
minji0608 <88191880+minjiLee68@users.noreply.github.com>
2023-05-12 13:13:17 +0900

    Initial Commit

---
+++ PLANNER/PLANNERApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLANNERApp.swift
+//  PLANNER
+//
+//  Created by 이민지 on 2023/05/12.
+//
+
+import SwiftUI
+
+@main
+struct PLANNERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
